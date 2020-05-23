868371c2bf96eb4b6f8392f528514d452555ec7d
neilah03 <cdcournoyer@gmail.com>
2020-05-22 22:41:30 -0700

    Updated for bonus opportunity

--- test_counter.py
+++ test_counter.py
@@ -6,3 +6,12 @@ def test_counter_inc():
   
 def test_counter_dec():
     assert counter.dec(5) == 4
+
+def test_counter_prod():
+    assert counter.prod(8, 8) == 64
+    
+def test_counter_square():
+    assert counter.square(6) == 36
+
+def test_counter_div():
+    assert counter.div(8, 4) == 2
